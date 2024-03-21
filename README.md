cde91ee081965909ab56ae93ceeb3ce594679a03
Shreshtha Pankaj <shreshtha.pankaj@colorado.edu>
2024-03-21 15:51:34 -0700

    Discord bot talking to chat gpt in NodeJs

---
+++ README.md
@@ -0,0 +1,25 @@
+# Simple discord bot connecting to chatGpt
+
+## How to use:
+
+### Setup NPM
+https://docs.npmjs.com/downloading-and-installing-node-js-and-npm  
+
+### Dicord Setup
+1. Setup a discord bot and make sure it has appropriate permissions (MESSAGE CONTENT INTENT on the bot page should be checked)
+2. Invite the bot to the channel
+3. Generate a token and update it in the .env(DISCORD_TOKEN) file
+
+### OpenAi Setup
+1. Register on openai portal, https://platform.openai.com/api-keys  
+2. Make sure you have avaiilable credits
+2. Generate a token and update it in the .env(OPEN_API_TOKEN) file
+
+### Running the service
+1. `npm install`
+2. `npm start`
+
+
+# Enjoy!!
+
+
---
+++ bot.js
@@ -0,0 +1,44 @@
+import { Client, GatewayIntentBits } from "discord.js";
+import 'dotenv/config'
+import OpenAI from 'openai';
+
+const token = `${process.env.DISCORD_TOKEN}`;
+const openai = new OpenAI({
+     apiKey: process.env.OPEN_API_TOKEN
+})
+
+const client = new Client({
+    intents:[
+        GatewayIntentBits.DirectMessages,
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
+    ]
+});
+client.on("ready", () =>{
+    console.log("Bot ready!");
+});
+
+async function callOpenAi(prompt) {
+    const completion = await openai.chat.completions.create({
+        messages: [{ role: "system", content: prompt }],      // change here 
+        model: "gpt-3.5-turbo",
+      });
+    
+    return completion;
+}
+
+client.on("messageCreate", function(discordMessage) {
+    if (discordMessage.author.bot) return;
+    discordMessage.reply(`Loading please wait...`); 
+
+    callOpenAi(discordMessage.content).then(response => {
+        if (response && response.choices && response.choices.length > 0){
+            discordMessage.reply(`${response.choices[0].message.content.trim()}`);
+        } else {
+            discordMessage.reply("Sorry can't connect to GPT");
+        }
+    });
+    return;
+ });
+client.login(token);
